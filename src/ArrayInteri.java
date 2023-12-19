da2eae034d17addd284d3b11fe0cb59460dada97
Vittorio Longo <vittorio.longo@itsrizzoli.it>
2023-12-19 12:04:03 +0100

    file.finale

--- src/ArrayInteri.java
+++ src/ArrayInteri.java
@@ -1,5 +1,30 @@
-public class App {
+import java.util.Scanner;
+
+public class ArrayInteri {
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeri;
+        int somma = 0;
+        int []array;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quanti numeri vuoi inserire?:");
+        numeri = scanner.nextInt();
+        array = new int [numeri];
+        for (int i = 0; i < numeri ; i++){
+            i = i +1;
+            System.out.println("Inserisci numero " +i);
+            i = i -1;
+            array[i] = scanner.nextInt();
+        }
+            System.out.println("Numeri in array: ");
+            for(int i = 0; i < numeri;i++){
+            System.out.println("  " +array[i]);
+        }
+        for(int i = 0; i < numeri; i ++){
+            somma = somma + array[i];
+        }
+        System.out.println("La somma dei numeri e'" +somma);
+        scanner.close();
     }
 }
